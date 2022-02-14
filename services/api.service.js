5baa5dca13ecc1ab92a246325ac8e0e3bbea9a4b
Dmytro Melnichenko <dmytro.melnichenko@gmail.com>
2022-02-14 10:27:22 +0200

    used axios package in api.service

--- services/api.service.js
+++ services/api.service.js
@@ -1,12 +1,23 @@
+import axios from 'axios';
 import {getKeyValue, TOKEN_DICTIONARY} from './storage.service.js'
-import https from 'https'
+//import https from 'https';
 const getWeather = async (city) => {
   const token = await getKeyValue(TOKEN_DICTIONARY.token);
   if (!token) {
     throw new Error('You need to set api key -t [API_KEY]')
   }
+  const {data} = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+    params: {
+      q: city,
+      appid: token,
+      lang: 'en',
+      units: 'metric'
+    }
+  });
+  return data;
+
   //const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API}`;
-  const url = new URL('https://api.openweathermap.org/data/2.5/weather');
+  /* const url = new URL('https://api.openweathermap.org/data/2.5/weather');
   url.searchParams.append('q', city);
   url.searchParams.append('appid', token);
   url.searchParams.append('lang', 'en');
@@ -20,7 +31,7 @@ const getWeather = async (city) => {
     response.on('end', () => {
       console.log(res);
     });
-  });
+  }); */
 };
 
 export {getWeather};
